e3e8f00ac7105ad8e74ffbe2748c59483cea975c
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-09-01 17:24:12 +0400

    Update enableRDP.ps1

--- enableRDP.ps1
+++ enableRDP.ps1
@@ -7,7 +7,7 @@ Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\W
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0 # Turn on RDP services
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name UserAuthentication -Value 1
 
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name 'SelectTransport' -Value 2 # 0 - TCP and UDP, 1 - Only TCP, 2 - TCP or UDP 
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name 'SelectTransport' -Value 0 # 0 - TCP and UDP, 1 - Only TCP, 2 - TCP or UDP 
 
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'fSingleSessionPerUser' -Value 0 # many sessions
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'MaxInstanceCount' -Value 4 # max sessions
