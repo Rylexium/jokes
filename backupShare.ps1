2bc9a1a0059574dc89248e0cc92c398c9fe49279
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-10-01 02:02:45 +0400

    Update backupShare.ps1

--- backupShare.ps1
+++ backupShare.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = "Stop"
+#$ErrorActionPreference = "Stop"
 
 chcp 65001
 
@@ -28,9 +28,17 @@ Write-Host "!!!!!! Done delete files from staging"
 
 $dirs = ls $theFolder
 $exclusionDirs = @("backup")
+$nameOfSmbPolicy="SMBRestrictFileCopySpeed"
 
-if((Get-NetQosPolicy | select -Property Name) -eq $null){
-    New-NetQosPolicy -Name "SMBRestrictFileCopySpeed" -SMB -ThrottleRateActionBitsPerSecond 100MB #limit for uploading
+if($nameOfSmbPolicy -in [array](Get-NetQosPolicy | select -Property Name)){
+    Write-Host "Delete Policy: $nameOfSmbPolicy"
+    Remove-NetQosPolicy -Name $nameOfSmbPolicy -Confirm:$false
+}
+
+
+if($nameOfSmbPolicy -in [array](Get-NetQosPolicy | select -Property Name)){
+    Write-Host "Add Policy: $nameOfSmbPolicy"
+    New-NetQosPolicy -Name "$nameOfSmbPolicy" -SMB -ThrottleRateActionBitsPerSecond 1600MB #limit for uploading (in mbit) 800Mbit = 100mbait
 }
 
 
@@ -46,24 +54,29 @@ ForEach($dir in $dirs){ #xcopy $theFolder\ $staging /s /e
     else{
         xcopy $dir.FullName $staging /s /e
     }
-
+    
 
 }
 $backupZipFile = $local_backup + "\" + (Get-Date -Format "dd-MM-yyyy").ToString() + ".zip"
 $backupZipFile
+if(Test-Path $backupZipFile) {
+    Remove-Item $backupZipFile -Force
+}
 
+if((Test-Path $staging)){
+    & 'C:\Program Files\7-Zip\7z.exe' a -bb3 -stm16 -y -mx5 -tzip -ssw -r0 $backupZipFile $staging\* # create archive and move to local storage
+    Write-Host "!!!!!! Done create zip archive"
+    Write-Host "!!!!!! Done copy zip archive to local"
 
-& 'C:\Program Files\7-Zip\7z.exe' a -mx2 -tzip -ssw -r0 $backupZipFile $staging\* # create archive and move to local storage
-Write-Host "!!!!!! Done create zip archive"
-Write-Host "!!!!!! Done copy zip archive to local"
+    Copy-Item $backupZipFile ($theFolder+"\backup") #send archive to share server
+    Write-Host "!!!!!! Done copy zip archive to \\172.17.250.10"
 
-
-Copy-Item $backupZipFile ($theFolder+"\backup") #send archive to share server
-Write-Host "!!!!!! Done copy zip archive to \\172.17.250.10"
-
-
-Remove-Item $staging\* -Recurse -Force
-Remove-Item $staging
-Write-Host "!!!!!! Done delete files from staging"
-
-Remove-NetQosPolicy -Name SMBRestrictFileCopySpeed
+    Remove-Item $staging\* -Recurse -Force
+    Remove-Item $staging
+    Write-Host "!!!!!! Done delete files from staging"
+    Remove-NetQosPolicy -Name $nameOfSmbPolicy -Confirm:$false
+    Write-Host "========================================================"
+}
+else{
+    Write-Host "!!!!!! Directory $staging is not exists."
+}
