e0b03d91a137ea9b0fe6ea9cfd64ef17c5e51367
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-10-01 20:17:17 +0400

    Create main.py

---
+++ backupShare/main.py
@@ -0,0 +1,29 @@
+import asyncio
+import sys
+
+import keyring
+from aiogram import Bot, types
+
+token = keyring.get_password("telegram", "api key")
+chat_id = int(keyring.get_password("telegram", "test_chat_id"))  # VOLS_CHANNEL_ID #test_chat_id
+
+bot = Bot(token=token)
+loop = asyncio.get_event_loop()
+
+
+def send_message(text, path_to_log, parse_mode="HTML"):
+    async def _async_send_message():
+        media_group = [types.InputMediaDocument(open(path_to_log, 'rb'), parse_mode=parse_mode)]
+        media_group[0].caption = text.rstrip('\n')
+        return await bot.send_media_group(chat_id=chat_id, media=media_group)
+    loop.run_until_complete(_async_send_message())
+
+
+def edit_pattern(msg):
+    return "<b>Backup (soc-files)</b>\n" \
+           f"🛑 Произошла ошибка во время: <b><i>{msg}</i></b>."
+
+
+#"архивации данных" "отправки архива на файловый сервер"
+send_message(edit_pattern(sys.argv[1]), sys.argv[2])
+send_message(edit_pattern(sys.argv[1]), sys.argv[2])
