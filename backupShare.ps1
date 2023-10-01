504a401127cf84d68f4ff0e7a75931f0498f62a0
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-10-01 19:08:45 +0400

    Update backupShare.ps1

--- backupShare.ps1
+++ backupShare.ps1
@@ -16,7 +16,8 @@ $staging = "C:\soc-files" #Tmp directory, here downloading all files from share 
 $localBackup = "C:\backup(172.17.250.10)" #Here save .zip file with all files from share server.
 
 $exclusionDirs = @("backup") #name of directory exclusion
-$nameOfSmbPolicy="SMBRestrictFileCopySpeed" #name of policy, what restrict smb(traffic) speed
+$nameOfSmbPolicy = "SMBRestrictFileCopySpeed" #name of policy, what restrict smb(traffic) speed
+$speedSMBUploadingBitsPerSecond = 1600MB
 
 
 function createDir($path) {
@@ -32,21 +33,25 @@ function removeFilesDir($path){
     }
 }
 
-createDir($localBackup)
-createDir($staging)
-#removeFilesDir($staging)
-
-
-
-if($nameOfSmbPolicy -in [array](Get-NetQosPolicy | select -Property Name)){ #check policy, if exists delete
-    Write-Host "Delete Policy: $nameOfSmbPolicy"
-    Remove-NetQosPolicy -Name $nameOfSmbPolicy -Confirm:$false
+function removeRestrictTrafficSMBPolicy($policy){
+    if($policy -in [array](Get-NetQosPolicy | select -Property Name)){ #check policy, if exists delete
+        Write-Host "Delete Policy: $policy"
+        Remove-NetQosPolicy -Name $policy -Confirm:$false
+    }
 }
+
+createDir($localBackup) #create dir if not exists
+createDir($staging)
+#removeFilesDir($staging) #remove all files, if exists staging
+
+
+removeRestrictTrafficSMBPolicy($nameOfSmbPolicy) #delete previous policy restrict speed smb
+
 
 
 if($nameOfSmbPolicy -in [array](Get-NetQosPolicy | select -Property Name)){ #create policy restrict smb speed
     Write-Host "Add Policy: $nameOfSmbPolicy"
-    New-NetQosPolicy -Name "$nameOfSmbPolicy" -SMB -ThrottleRateActionBitsPerSecond 1600MB #limit for uploading (in mbit) 800Mbit = 100mbait
+    New-NetQosPolicy -Name "$nameOfSmbPolicy" -SMB -ThrottleRateActionBitsPerSecond $speedSMBUploadingBitsPerSecond  #limit for uploading (in mbit) 800Mbit = 100mbait
 }
 
 
@@ -64,7 +69,7 @@ ForEach($dir in (ls $theFolder)){ #download all dir and files from share server 
     }
 }
 
-$backupZipFile = $localBackup + "\" + (Get-Date -Format "dd-MM-yyyy").ToString() + ".7z" #example path: C:\stage\25-09-2023.zip
+$backupZipFile = $localBackup + "\" + (Get-Date -Format "dd-MM-yyyy").ToString() + ".7z" #example path: C:\stage\25-09-2023.7z
 $backupZipFile
 if(Test-Path $backupZipFile) { #remove file with same name
     Remove-Item $backupZipFile -Force
@@ -75,28 +80,31 @@ if(-not (Test-Path $staging)){ #staging not exists throw exception
 }
 
 
-if(Test-Path $backupZipFile){
+if(Test-Path $backupZipFile){ #delete previous archive if exists, this script create new archive
     Remote-Item $backupZipFile -Force
 }
 
 # -bb3 - full log, -stm16 - 16 threats, -y - access all dialogs, -mx5 - step of compress, -tzip - type (zip) of archive, -ssw - analogue "force", -r0 - recursive all directories
-try{ #mx2 40gb -> 30gb, mx5 40gb -> ?gb, mx9 40gb ->?gb
+try {
     & 'C:\Program Files\7-Zip\7z.exe' a -bb2 -stm32 -y -mx9 -t7z -ssw -r0 $backupZipFile $staging # create archive and move to local storage
-}catch {
+    Write-Host "!!!!!! Done create zip archive"
+    Write-Host "!!!!!! Done copy zip archive to local"
+} catch {
     Write-Host "Error when archiving data..."
 }
 
-Write-Host "!!!!!! Done create zip archive"
-Write-Host "!!!!!! Done copy zip archive to local"
+try {
+    Copy-Item $backupZipFile ($theFolder+"\backup") #send archive to share server
+    Write-Host "!!!!!! Done copy zip archive to \\172.17.250.10"
 
-Copy-Item $backupZipFile ($theFolder+"\backup") #send archive to share server
-Write-Host "!!!!!! Done copy zip archive to \\172.17.250.10"
+    #removeFilesDir($staging)
+    #Remove-Item $staging
+    Write-Host "!!!!!! Done delete files and directory from $staging"
 
-#removeFilesDir($staging)
-#Remove-Item $staging
-Write-Host "!!!!!! Done delete files and directory from $staging"
-
-Remove-NetQosPolicy -Name $nameOfSmbPolicy -Confirm:$false
-$timeExecutionBackupScript.Stop()
-$timeExecutionBackupScript.Elapsed
-Write-Host "========================================================"
+    removeRestrictTrafficSMBPolicy($nameOfSmbPolicy)
+    $timeExecutionBackupScript.Stop()
+    $timeExecutionBackupScript.Elapsed
+    Write-Host "========================================================"
+} catch {
+    Write-Host "Error when send archive to share server..."
+}
