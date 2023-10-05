1ef6c8f5c2f206f4c9b6f9df919dcfa66f88e405
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-10-05 06:57:48 +0400

    Update enableRDP.ps1

--- enableRDP.ps1
+++ enableRDP.ps1
@@ -9,7 +9,7 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\W
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name 'SelectTransport' -Value 0 # 0 - TCP and UDP, 1 - Only TCP, 2 - TCP or UDP 
 
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'fSingleSessionPerUser' -Value 0 # many sessions
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'fSingleSessionPerUser' -Value 1 # many sessions
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name 'MaxInstanceCount' -Value 4 # max sessions
 
 #add to firewall
@@ -42,49 +42,52 @@ if ($result -eq 'Yes') {
 	net localgroup "administrators" /add $username
 }
 
-
-$scriptText = "
-#restart services and set many sessions on host
-Stop-Service UmRdpService -Force
-Stop-Service TermService -Force
-$termsrv_dll_acl = Get-Acl c:\windows\system32\termsrv.dll
-Copy-Item c:\windows\system32\termsrv.dll c:\windows\system32\termsrv.dll.copy
-takeown /f c:\windows\system32\termsrv.dll
-$new_termsrv_dll_owner = (Get-Acl c:\windows\system32\termsrv.dll).owner
-cmd /c 'icacls c:\windows\system32\termsrv.dll /Grant $($new_termsrv_dll_owner):F /C'
-# search for a pattern in termsrv.dll file 
-$dll_as_bytes = Get-Content c:\windows\system32\termsrv.dll -Raw -Encoding byte
-$dll_as_text = $dll_as_bytes.forEach('ToString', 'X2') -join ' '
-$patternregex = ([regex]'39 81 3C 06 00 00(\s\S\S){6}')
-$patch = 'B8 00 01 00 00 89 81 38 06 00 00 90'
-$checkPattern=Select-String -Pattern $patternregex -InputObject $dll_as_text
-If ($checkPattern -ne $null) {
-    $dll_as_text_replaced = $dll_as_text -replace $patternregex, $patch
-}
-Elseif (Select-String -Pattern $patch -InputObject $dll_as_text) {
-    Write-Output 'The termsrv.dll file is already patched, exiting'
-    Exit
-}
-else { 
-    Write-Output 'Pattern not found'
-}
-# patching termsrv.dll
-[byte[]] $dll_as_bytes_replaced = -split $dll_as_text_replaced -replace '^', '0x'
-Set-Content c:\windows\system32\termsrv.dll.patched -Encoding Byte -Value $dll_as_bytes_replaced
-# comparing two files 
-fc.exe /b c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll
-# replacing the original termsrv.dll file 
-Copy-Item c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll -Force
-Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
-Start-Service UmRdpService
-Start-Service TermService"
-
-
-#add this script to scheduler task
-
-#write script to shell:startup
-$path='C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\enableRDP.ps1';
-$scriptText >> $path
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$result = [System.Windows.Forms.MessageBox]::Show('Patch termsrv.dll in scheduler?' , "Question" , 4)
+if ($result -eq 'Yes') {
+	$scriptText = "
+	#restart services and set many sessions on host
+	Stop-Service UmRdpService -Force
+	Stop-Service TermService -Force
+	$termsrv_dll_acl = Get-Acl c:\windows\system32\termsrv.dll
+	Copy-Item c:\windows\system32\termsrv.dll c:\windows\system32\termsrv.dll.copy
+	takeown /f c:\windows\system32\termsrv.dll
+	$new_termsrv_dll_owner = (Get-Acl c:\windows\system32\termsrv.dll).owner
+	cmd /c 'icacls c:\windows\system32\termsrv.dll /Grant $($new_termsrv_dll_owner):F /C'
+	# search for a pattern in termsrv.dll file 
+	$dll_as_bytes = Get-Content c:\windows\system32\termsrv.dll -Raw -Encoding byte
+	$dll_as_text = $dll_as_bytes.forEach('ToString', 'X2') -join ' '
+	$patternregex = ([regex]'39 81 3C 06 00 00(\s\S\S){6}')
+	$patch = 'B8 00 01 00 00 89 81 38 06 00 00 90'
+	$checkPattern=Select-String -Pattern $patternregex -InputObject $dll_as_text
+	If ($checkPattern -ne $null) {
+	    $dll_as_text_replaced = $dll_as_text -replace $patternregex, $patch
+	}
+	Elseif (Select-String -Pattern $patch -InputObject $dll_as_text) {
+	    Write-Output 'The termsrv.dll file is already patched, exiting'
+	    Exit
+	}
+	else { 
+	    Write-Output 'Pattern not found'
+	}
+	# patching termsrv.dll
+	[byte[]] $dll_as_bytes_replaced = -split $dll_as_text_replaced -replace '^', '0x'
+	Set-Content c:\windows\system32\termsrv.dll.patched -Encoding Byte -Value $dll_as_bytes_replaced
+	# comparing two files 
+	fc.exe /b c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll
+	# replacing the original termsrv.dll file 
+	Copy-Item c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll -Force
+	Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
+	Start-Service UmRdpService
+	Start-Service TermService"
+	
+	
+	#add this script to scheduler task
+	
+	#write script to shell:startup
+	$path='C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\enableRDP.ps1';
+	$scriptText >> $path
+ }
 
 #execute script
 Stop-Service UmRdpService -Force
